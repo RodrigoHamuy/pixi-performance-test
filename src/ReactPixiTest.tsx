4fed7eb749ee4c8407364121180fc2adbb975636
oyar <hamuyrodrigo@gmail.com>
2019-06-08 19:48:42 +0100

    Add: PIXI test

---
+++ src/ReactPixiTest.tsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default () => {
+  ReactDOM.render(<div>Hello world</div>, document.getElementById('root'));
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { PixiTest } from './PixiTest';
+import * as dat from 'dat.gui';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const state = { sprites: 10 };
+
+const test = new PixiTest();
+
+test.totalSprites(state.sprites);
+
+const gui = new dat.GUI();
+gui.add(state, 'sprites', 15).onChange(function(val: number){
+  test.totalSprites(val);
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
