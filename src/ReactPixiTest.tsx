c3a179e605f9b9a91417a7987be1cb894a42f527
oyar <hamuyrodrigo@gmail.com>
2019-06-08 20:53:52 +0100

    Add: React test

--- src/ReactPixiTest.tsx
+++ src/ReactPixiTest.tsx
@@ -1,6 +1,52 @@
+import { Stage, AppConsumer } from "@inlet/react-pixi";
+import dat from 'dat.gui';
+import * as pixi from "pixi.js";
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import bunny from './bunny.png';
+import { RotatingSprite } from "./RotatingSprite";
 
-export default () => {
-  ReactDOM.render(<div>Hello world</div>, document.getElementById('root'));
-};
+interface iReactPixiTestState {
+  sprites: number;
+  texture: pixi.Texture;
+}
+
+
+export class ReactPixiTest extends Component<{}, iReactPixiTestState>{
+
+  state: iReactPixiTestState = {
+    sprites: 0,
+    texture: pixi.Texture.from(bunny),
+  };
+
+  constructor(props: {}) {
+    super(props);
+    const state = { sprites: 10 };
+    const gui = new dat.GUI();
+
+    gui.add(state, 'sprites', 15).onChange((val: number) => {
+      this.setState({ sprites: val });
+    });
+
+    this.state.sprites = state.sprites;
+
+  }
+  render() {
+    return (
+      <Stage
+        width={window.innerWidth < 800 ? window.innerWidth : 800}
+        height={window.innerHeight < 600 ? window.innerHeight : 600}
+      >
+        {
+          new Array(this.state.sprites).fill(true).map((v, i) => (
+            <AppConsumer key={i}>{ app => (
+              <RotatingSprite
+                app={app}
+                texture={this.state.texture}
+              />
+            )}</AppConsumer>
+          ))
+        }
+      </Stage>
+    );
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,31 @@
-import * as serviceWorker from './serviceWorker';
-import { PixiTest } from './PixiTest';
 import * as dat from 'dat.gui';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { PixiTest } from './PixiTest';
+import { ReactPixiTest } from './ReactPixiTest';
+import * as serviceWorker from './serviceWorker';
 
+// runReactTest();
+runPixiTest();
 
-const state = { sprites: 10 };
+function runReactTest(){
+  ReactDOM.render(<ReactPixiTest />, document.getElementById('root'));
+}
 
-const test = new PixiTest();
+function runPixiTest() {
 
-test.totalSprites(state.sprites);
+  const state = { sprites: 10 };
+  const gui = new dat.GUI();
 
-const gui = new dat.GUI();
-gui.add(state, 'sprites', 15).onChange(function(val: number){
-  test.totalSprites(val);
-});
+  const pixiTest = new PixiTest();
+
+  pixiTest.totalSprites(state.sprites);
+
+  gui.add(state, 'sprites', 15).onChange(function (val: number) {
+    pixiTest.totalSprites(val);
+  });
+
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
